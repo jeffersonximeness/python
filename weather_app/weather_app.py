37fa30d8dddee1d2807807a35915669481495291
Jefferson <jeffersonximenes1@gmail.com>
2021-05-04 21:35:25 -0300

    Forecast for next 5 days

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -1,9 +1,10 @@
 import requests 
 import json
 import pprint
+from datetime import date
 
 API_KEY = ''
-
+dias = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 def obter_coordenadas():
     r = requests.get('http://www.geoplugin.net/json.gp')
@@ -47,12 +48,48 @@ def obter_tempo_agora(codigo_local, nome_local):
 
     return info_clima
 
+def previsao_futura_json(codigo_local):
+    future_conditions = f'http://dataservice.accuweather.com/forecasts/v1/daily/5day/{codigo_local}?apikey={API_KEY}&language=pt-br&metric=True'
+    r = requests.get(future_conditions)
+
+    if r.status_code != 200:
+        print('Não foi possível obter a previsão')
+
+    future_conditions_response = json.loads(r.text)
+    
+    return future_conditions_response['DailyForecasts']
+
+def previsao_futura(json_previsao):
+    info_5_dias = []
+
+    for dia in json_previsao:
+        clima_dia = {}
+        clima_dia['max'] = dia['Temperature']['Maximum']['Value']
+        clima_dia['min'] = dia['Temperature']['Minimum']['Value']
+        clima_dia['clima'] = dia['Day']['IconPhrase']
+        clima_dia['dia'] = dia['EpochDate']
+        
+        info_5_dias.append(clima_dia)
+
+    return info_5_dias 
+
 
 if __name__ == '__main__':
     coordenadas = obter_coordenadas()
     local = obter_codigo_local(coordenadas['lat'], coordenadas['long'])
+
     clima_atual = obter_tempo_agora(local['codigo_local'], local['nome_local'])
     
     print('Clima atual em: {}'.format(clima_atual['nome_local']))
     print('{}'.format(clima_atual['texto_clima']))
-    print('Temperatura: {} {}C'.format(str(clima_atual['temperatura']), '\xb0'))
+    print('Temperatura: {} {}C\n'.format(str(clima_atual['temperatura']), '\xb0'))
+
+    json_previsao = previsao_futura_json(local['codigo_local'])
+    lista_dias = previsao_futura(json_previsao)
+    
+    for dia in lista_dias:
+        print(dia['dia'])
+        print('Mínima: {} {}C'.format(str(dia['min']), '\xb0'))
+        print('Máxima: {} {}C'.format(str(dia['max']), '\xb0'))
+        print('Clima: {}'.format(str(dia['clima'])))
+        print('----------------------------------')
